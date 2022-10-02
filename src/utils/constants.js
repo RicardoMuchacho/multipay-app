885857aae34460002f07243292a3dc15e99eeba0
rickmuch <ricardojrmuchacho@gmail.com>
2022-10-02 18:21:28 -0400

    utils folder updated with smart contract data

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
-export const multipayTokenAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
-export const multipayAbi = ''
+import multipayToken from './Multipay.json'
+
+export const multipayAbi = multipayToken.abi
 export const testnet = 'goerli'
 export const contract = '0x5A5E76084a69dd786BEA7f87AA20204d2F791576'
